868484fb62d59468b27aca6891640b5d36ac8c9d
Ankit Mishra <ankit1703@gmail.com>
2023-01-30 08:27:04 +0530

    New function added.

--- GolangProjects/Cards/main.go
+++ GolangProjects/Cards/main.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	d1 := deck{"1", "2"}
+	fmt.Println(d1)
 	cards := newDeck()
-	cards.print()
+	hand, remainingDeck := deal(cards, 5)
+	hand.print()
+	remainingDeck.print()
+
 }
