c9effb6e9d4bd6b77abeeedba5df9b4ffc10afb7
Ankit Mishra <ankit1703@gmail.com>
2023-03-31 13:34:01 +0530

    More routes

--- go-hexa-microservice/Banking/app/app.go
+++ go-hexa-microservice/Banking/app/app.go
@@ -12,8 +12,11 @@ func Start() {
 	router := mux.NewRouter()
 
 	// Routes
-	router.HandleFunc("/greet", greet)
-	router.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+
+	router.HandleFunc("/customers/", createCustomer).Methods(http.MethodPost)
 
 	// Server Start
 	log.Fatal(http.ListenAndServe("localhost:8000", router))
