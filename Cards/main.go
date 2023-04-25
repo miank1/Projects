97a3722f2025fd3aa71540906b517c854f907cb3
Ankit Mishra <ankit.mishra@nupay.co.in>
2023-04-25 12:49:10 +0530

    Initial Commit Card Project

---
+++ Cards/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{
+		"Ace Of Diamnonds", newCard()}
+
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+	fmt.Println(cards)
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
