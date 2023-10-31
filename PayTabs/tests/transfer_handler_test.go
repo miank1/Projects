14ab74b7a54e9c3cda9ac1df8cf620cb864e303b
Ankit Mishra <ankit1703@gmail.com>
2023-10-31 17:20:19 +0530

    some code fix.

--- PayTabs/tests/transfer_handler_test.go
+++ PayTabs/tests/transfer_handler_test.go
@@ -1,1 +1,13 @@
 package tests
+
+import (
+	"accounts/handlers"
+	"testing"
+)
+
+func TestTransfer(t *testing.T) {
+	result := handlers.TransferHandler(5, 2)
+	if result != 3 {
+		t.Errorf("Expected 3, but got %d", result)
+	}
+}
