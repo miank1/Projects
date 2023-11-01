e9602619ec76cb3f7e05e973ff18aeaa2e8345c0
Ankit Mishra <ankit1703@gmail.com>
2023-11-01 09:03:30 +0530

    Added Testcases.

--- PayTabs/tests/account_handler_test.go
+++ PayTabs/tests/account_handler_test.go
@@ -1,1 +1,4 @@
 package tests
+
+// For this accont handler no testcase has been written since it's being read on
+// runtime from a URL.
--- PayTabs/tests/transfer_handler_test.go
+++ PayTabs/tests/transfer_handler_test.go
@@ -2,12 +2,61 @@ package tests
 
 import (
 	"accounts/handlers"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestTransfer(t *testing.T) {
-	result := handlers.TransferHandler(5, 2)
-	if result != 3 {
-		t.Errorf("Expected 3, but got %d", result)
+func TestTransferHandler(t *testing.T) {
+	// Create a new HTTP request with a JSON payload
+	transferData := map[string]interface{}{
+		"from":   "fd796d75-1bcf-4a95-bf1a-f7b296adb79f",
+		"to":     "ccd1e5cc-c798-4407-883f-f2c62e0d7106",
+		"amount": 500.0,
 	}
+	reqBody, _ := json.Marshal(transferData)
+	req, err := http.NewRequest("POST", "/transfer", bytes.NewBuffer(reqBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a response recorder to record the response
+	rr := httptest.NewRecorder()
+
+	// Call the TransferHandler with the request and response recorder
+	handlers.TransferHandler(rr, req)
+
+	// Check the response status code
+	if rr.Code == http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, rr.Code)
+	}
+
+}
+
+func TestTransferHandler_1(t *testing.T) {
+	// Create a new HTTP request with a JSON payload
+	transferData := map[string]interface{}{
+		"from":   "3d253e29-8785-464f-8fa0-9e4b57699db9",
+		"to":     "17f904c1-806f-4252-9103-74e7a5d3e340",
+		"amount": 2000.0, // Transferring money more than the balance
+	}
+	reqBody, _ := json.Marshal(transferData)
+	req, err := http.NewRequest("POST", "/transfer", bytes.NewBuffer(reqBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a response recorder to record the response
+	rr := httptest.NewRecorder()
+
+	// Call the TransferHandler with the request and response recorder
+	handlers.TransferHandler(rr, req)
+
+	// Check the response status code
+	if rr.Code != http.StatusBadRequest {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, rr.Code)
+	}
+
 }
