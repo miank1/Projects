7bd566e0f5868bf7a8b9e5166b1360cd52e89772
Ankit Mishra <ankit1703@gmail.com>
2023-12-27 18:38:42 +0530

    Added json function.

--- GreenlightAPI/cmd/api/healthcheck.go
+++ GreenlightAPI/cmd/api/healthcheck.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s \n", app.config.env)
-	fmt.Fprintf(w, "version: %s \n", version)
+	// create a map which holds the information that we want to send in the response
+
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	// pass the map to json.Marshal() function. This returns a []byte slice
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write(js)
 }
