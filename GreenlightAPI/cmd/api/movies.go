2f8ec739667f6be0564a475123122b5218baa0dd
Ankit Mishra <ankit1703@gmail.com>
2023-12-28 07:17:18 +0530

    Created a movie.go under data folder removed from api.

--- GreenlightAPI/cmd/api/movies.go
+++ GreenlightAPI/cmd/api/movies.go
@@ -2,35 +2,45 @@ package main
 
 import (
 	"fmt"
+	"greenlight/internal/data"
 	"net/http"
-	"strconv"
 	"time"
-
-	"github.com/julienschmidt/httprouter"
 )
-
-type Movie struct {
-	ID        int64
-	CreatedAt time.Time
-	Title     string
-	Year      int32
-	Runtime   int32
-	Genres    []string
-	Version   int32
-}
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "create a new movie")
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
+	id, err := app.readIDParam(r)
 
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
+
+	// params := httprouter.ParamsFromContext(r.Context())
+
+	// id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	// if err != nil || id < 1 {
+	// 	http.NotFound(w, r)
+	// 	return
+	// }
+
+	// fmt.Fprintf(w, "show the details of movie %d\n", id)
 }
