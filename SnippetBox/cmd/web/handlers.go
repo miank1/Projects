df783917b5a1e0cc1746ecfee26439c97a78715a
Ankit Mishra <ankit1703@gmail.com>
2024-01-05 23:48:30 +0530

    Rewrote the home api.

--- SnippetBox/cmd/web/handlers.go
+++ SnippetBox/cmd/web/handlers.go
@@ -2,18 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	w.Write([]byte("Hello from Snippetbox")) // why conversion to byte
-}
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
@@ -33,4 +26,31 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
+		"/home/miank/workspace/GitHub/Projects/SnippetBox/ui/html/home.page.tmpl",
+		"/home/miank/workspace/GitHub/Projects/SnippetBox/ui/html/base.page.tmpl",
+		"/home/miank/workspace/GitHub/Projects/SnippetBox/ui/html/footer.page.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+
 }
