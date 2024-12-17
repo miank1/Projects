6c26a982d0e88ba60a3872bf63aba2d9c8a02e99
Ankit Mishra <ankit1703@gmail.com>
2024-12-17 23:20:15 +0530

    added code to handers.go

--- SnippetBox/cmd/web/handlers.go
+++ SnippetBox/cmd/web/handlers.go
@@ -1,1 +1,27 @@
 package web
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	t, err := template.ParseFiles("./ui/html/homepage.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+
+}
