2b249eff7b96691bfa83a94374651d7531cd0958
Ankit Mishra <ankit1703@gmail.com>
2025-05-08 17:23:33 +0530

    Code updated in create and view
    Validation checks added and query string.

--- SnippetBox/main.go
+++ SnippetBox/main.go
@@ -4,17 +4,37 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 	w.Write([]byte("Display a specific snippet ..."))
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+
 	w.Write([]byte("Create a new snippet"))
+
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from SnippetBox"))
 }
 
