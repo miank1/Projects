68a538f79cb2fbd8a550d3adbab6acb25d907397
Ankit Mishra <ankit1703@gmail.com>
2025-05-25 13:44:31 +0530

    Added a new helper method Writejson

--- GreeenlightAPI/cmd/api/healthcheck.go
+++ GreeenlightAPI/cmd/api/healthcheck.go
@@ -10,9 +10,26 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	fmt.Fprintf(w, "environment : %s\n ", app.config.env)
 	fmt.Fprintf(w, "version: %s\n", version)
 
-	js := `{"status": "avaialble", "environment":"%q", "version":%q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and coud not prcoess your request", http.StatusInternalServerError)
+	}
+
+	// js, err := json.Marshal(data)
+	// if err != nil {
+	// 	app.logger.Println(err)
+	// 	http.Error(w, "The server encountered a problem and coud not prcoess your request", http.StatusInternalServerError)
+	// }
+
+	// js = append(js, '\n')
+
+	// w.Header().Set("Content-Type", "application/json")
+	// w.Write(js)
 }
